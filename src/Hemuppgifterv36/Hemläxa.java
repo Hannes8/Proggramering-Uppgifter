d1c9d9b54e3325e43a0f515abd3b457c9d93f6ce
Hannes8 <hannes.kaulio@gmail.com>
2019-09-07 12:31:18 +0200

    Create Hemläxa.java

---
+++ src/Hemuppgifterv36/Hemläxa.java
@@ -0,0 +1,80 @@
+package Hemuppgifterv36;
+
+import java.util.Scanner;
+
+public class Hemläxa {
+	static int[] heltal = {31,54,12,4,6,787,5,43,2,6,878,6};
+	static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		uppgift1();
+		
+		uppgift2();
+		
+		
+		
+	}
+	
+	// 1. Skapa en metod som tar in ett okänt antal heltal och returnerar deras summa.
+	public static void uppgift1() {
+		int summa=0;
+		
+		for (int i = 0; i < heltal.length; i++) {
+		
+			summa = summa + heltal[i];
+		} 
+		
+
+		
+		System.out.println("-1- Summan är: "+summa);
+		
+		
+	}
+	// 2. Skapa en rekursiv metod som tar in en sträng och returnerar den baklänges.
+	public static void uppgift2() {
+		String ordbaklänges ="";
+		
+		System.out.println("Skriv in ett ord!");
+		
+		String ord=input.next();
+		for (int i = ord.length()-1; i >= 0; i--) {
+		char bokstav=ord.charAt(i);
+		ordbaklänges=ordbaklänges+bokstav;
+			
+		}
+		System.out.println(ordbaklänges);
+	}
+	// 3. Skapa en generisk metod som tar in ett okänt antal objekt och returnerar en array av dem.
+	public static void uppgift3() {
+		
+		
+		
+		
+	}
+	// 4. Skapa en klass Box som tar in ett okänt antal objekt och lagrar dem.
+public static void uppgift4() {
+		
+		
+		
+	}
+// 5. Skapa en metod i klassen Box som kan jämföra ifall två stycken Boxar innehåller samma datatyp.*
+public static void uppgift5() {
+	
+	
+	
+}
+
+	// 6. Skapa en rekursiv metod som tar in ett ord och skriver ut alla anagram av ordet.*
+	
+	public static void uppgift6() {
+	
+		
+		
+	}
+	// 7. Skapa Application som rekursivt ritar ut ett fibonacci-träd.*
+	public static void uppgift7() {
+		
+		
+	}
+
+}
